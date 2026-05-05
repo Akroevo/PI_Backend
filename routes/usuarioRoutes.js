df372922bd16646555e00942f26443f56bc8d703
Otavio Augusto <otavio322@gmail.com>
2026-05-05 15:56:12 -0300

    Colocando SuperAdmin

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -4,11 +4,11 @@ const { autorizar, apenasProprioUsuario } = require('../middlewares/auth');
 
 router.get('/',    autorizar('superadmin'), ctrl.getAll);
 router.get('/:id',  autorizar('superadmin'), ctrl.getById);
-router.post('/',   autorizar('superadmin'), ctrl.create);
+router.post('/',    ctrl.create);
 router.put('/:id', autorizar('superadmin'), ctrl.update);
 router.delete('/:id', autorizar('superadmin'), ctrl.remove);
 
 
-//router.put('/:id/senha', apenasProprioUsuario, ctrl.updateSenha);
+router.put('/:id/senha', apenasProprioUsuario, ctrl.updateSenha);
 
 module.exports = router;

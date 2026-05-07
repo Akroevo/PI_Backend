5939d898a020b33d3ac5a6531749e22fedcea196
Otavio322 <93688360+Otavio322@users.noreply.github.com>
2026-05-07 10:07:06 -0300

    Acoplando os espaços

--- routes/coordenadorRoute.js
+++ routes/coordenadorRoute.js
@@ -2,15 +2,14 @@ const router = require('express').Router();
 const ctrl = require('../controllers/coordenadorController');
 const { autorizar, apenasProprioCoordenador } = require('../middlewares/auth');
 
-router.get('/',    autorizar('superadmin'), ctrl.getAll);
+router.get('/', autorizar('superadmin'), ctrl.getAll);
 router.get('/:id', autorizar('superadmin'), ctrl.getById);
-router.post('/',   autorizar('superadmin'), ctrl.create);
+router.post('/', autorizar('superadmin'), ctrl.create);
 router.put('/:id', autorizar('superadmin'), ctrl.update);
 router.delete('/:id', autorizar('superadmin'), ctrl.remove);
-
-router.put('/:id/atividades/:idAtividade/avaliar', coordenadorController.avaliarAtividade);
-router.get('/:id/cursos',             apenasProprioCoordenador, ctrl.getCursos);
-router.post('/:id/cursos',            autorizar('superadmin'), ctrl.addCurso);
+router.put('/:id/atividades/:idAtividade/avaliar', autorizar('coordenador'), ctrl.avaliarAtividade);
+router.get('/:id/cursos', apenasProprioCoordenador, ctrl.getCursos);
+router.post('/:id/cursos', autorizar('superadmin'), ctrl.addCurso);
 router.delete('/:id/cursos/:idCurso', autorizar('superadmin'), ctrl.removeCurso);
 
 module.exports = router;

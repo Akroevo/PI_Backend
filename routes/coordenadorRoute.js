6fbd3df7c999a7a14409b38a3d9447d5e8ced5ba
Otavio Augusto <otavio322@gmail.com>
2026-05-07 03:17:23 -0300

    Autorizando o coordenador

--- routes/coordenadorRoute.js
+++ routes/coordenadorRoute.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/coordenadorController');
-const { autorizar } = require('../middlewares/auth');
-
+const { autorizar, apenasProprioCoordenador } = require('../middlewares/auth');
 
 router.get('/',    autorizar('superadmin'), ctrl.getAll);
 router.get('/:id', autorizar('superadmin'), ctrl.getById);
@@ -9,7 +8,8 @@ router.post('/',   autorizar('superadmin'), ctrl.create);
 router.put('/:id', autorizar('superadmin'), ctrl.update);
 router.delete('/:id', autorizar('superadmin'), ctrl.remove);
 
-router.get('/:id/cursos',             autorizar('superadmin'), ctrl.getCursos);
+
+router.get('/:id/cursos',             apenasProprioCoordenador, ctrl.getCursos);
 router.post('/:id/cursos',            autorizar('superadmin'), ctrl.addCurso);
 router.delete('/:id/cursos/:idCurso', autorizar('superadmin'), ctrl.removeCurso);
 

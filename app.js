b0517c929bc5ae09f7574d671a799b6ecd3a51bb
Otavio Augusto <otavio322@gmail.com>
2026-05-07 01:39:21 -0300

    atualizando APP

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 const { autorizar } = require('./middlewares/auth');
-const app = express();
 const { logger, logsEmMemoria } = require('./middlewares/logger');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://pifrontend.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-
 app.use(morgan('dev', {
   stream: { write: (msg) => logger.info(msg.trim()) }
 }));
@@ -25,11 +31,10 @@ app.use('/api/coordenadores', require('./routes/coordenadorRoute'));
 app.use('/api/submissoes',    require('./routes/submissaoRoutes'));
 app.use('/api/certificados',  require('./routes/certificadoRoutes'));
 app.use('/api/notificacoes',  require('./routes/notificacaoRoutes'));
-app.use('/api/superadmins', require('./routes/superAdminRoutes'));
-
+app.use('/api/superadmins',   require('./routes/superAdminRoutes'));
 
 app.get('/api/logs', autorizar('superadmin'), (req, res) => {
   res.json(logsEmMemoria);
-
 });
-  module.exports = app;
+
+module.exports = app;

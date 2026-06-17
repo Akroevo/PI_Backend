5e8903e143bd8b97091536ad57a12cf4c61df085
Otavio Augusto <otavio322@gmail.com>
2026-06-17 00:12:35 -0300

    Mudança necessaria

--- database/db.js
+++ database/db.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
-const fs = require('fs');
-const path = require('path');
 
 
 const pool = mysql.createPool({
@@ -11,27 +9,11 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT, 
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 4,
   multipleStatements: true
 });
 
 
-(async () => {
-  try {
-    const conn = await pool.getConnection();
-    console.log('Conectado ao banco para verificar estrutura...');
-    
-    const schemaPath = path.join(__dirname, '../sql/schema.sql');
-    if (fs.existsSync(schemaPath)) {
-      const schemaSQL = fs.readFileSync(schemaPath, 'utf8');
-      await conn.query(schemaSQL);
-      console.log('Estrutura do banco verificada com sucesso!');
-    }
-    conn.release();
-  } catch (err) {
-    console.error('Erro ao preparar banco:', err.message);
-  }
-})();
 
 
 module.exports = pool;

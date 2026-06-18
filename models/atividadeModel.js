e2afcab63a1d0bbeb0400eecab47c0f38b6c80ac
Otavio322 <93688360+Otavio322@users.noreply.github.com>
2026-06-18 08:22:53 -0300

    voltando para o clever

--- models/atividadeModel.js
+++ models/atividadeModel.js
@@ -1,11 +1,8 @@
 const db = require('../database/db');
-
 const Atividade = {
   findAll: () => db.query('SELECT * FROM atividadecomplementar'),
-
   findById: (id) =>
     db.query('SELECT * FROM atividadecomplementar WHERE idAtividade = ?', [id]),
-
   findByAluno: (mat) =>
     db.query(
       `SELECT a.*, s.idSubmissao, s.status AS statusSubmissao, s.observacao,
@@ -15,7 +12,15 @@ const Atividade = {
        WHERE a.aluno_matricula = ?`,
       [mat]
     ),
-
+  findByCoordenador: (idCoordenador) =>
+    db.query(
+      `SELECT DISTINCT a.* FROM atividadecomplementar a
+       JOIN aluno al ON al.matricula = a.aluno_matricula
+       JOIN aluno_curso ac ON ac.aluno_matricula = al.matricula
+       JOIN coordenador_curso cc ON cc.curso_idCurso = ac.curso_idCurso
+       WHERE cc.coordenador_idCoordenador = ?`,
+      [idCoordenador]
+    ),
   create: (data) => db.query(
     `INSERT INTO atividadecomplementar
       (codigo, titulo, descricao, cargaHorariaSolicitada,
@@ -24,7 +29,6 @@ const Atividade = {
     [data.codigo, data.titulo, data.descricao,
      data.cargaHorariaSolicitada, data.aluno_matricula, data.regra_idRegra]
   ),
-
   update: (id, data) => db.query(
     `UPDATE atividadecomplementar
      SET codigo=?, titulo=?, descricao=?,
@@ -33,10 +37,8 @@ const Atividade = {
     [data.codigo, data.titulo, data.descricao,
      data.cargaHorariaSolicitada, data.regra_idRegra, id]
   ),
-
   delete: (id) =>
     db.query('DELETE FROM atividadecomplementar WHERE idAtividade = ?', [id]),
-
   avaliar: (id, data) => db.query(
     `UPDATE atividadecomplementar
      SET status=?, cargaHorariaAprovada=?, observacao=?, coordenador_idCoordenador=?
@@ -44,5 +46,4 @@ const Atividade = {
     [data.status, data.cargaHorariaAprovada, data.observacao, data.coordenador_idCoordenador, id]
   )
 };
-
 module.exports = Atividade;

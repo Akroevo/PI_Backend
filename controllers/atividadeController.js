d358a62e1c771c2880e5aebf425d61985369af31
Otavio322 <93688360+Otavio322@users.noreply.github.com>
2026-06-18 08:14:42 -0300

    Código quebrado

--- controllers/atividadeController.js
+++ controllers/atividadeController.js
@@ -1,34 +1,28 @@
 const Atividade = require('../models/atividadeModel');
 const { error: logError } = require('../middlewares/logger');
 
-async function getAll(req, res) {
+exports.getAll = async function (req, res) {
   try {
     const user = req.user;
-
     const atividades = await Atividade.find()
       .populate('alunoId');
 
-    
     if (user.role === 'superadmin') {
       return res.json(atividades);
     }
 
-    
     if (user.role === 'coordenador') {
       const filtradas = atividades.filter(a =>
         a.alunoId &&
         a.alunoId.coordenadorId == user.id
       );
-
       return res.json(filtradas);
     }
-
     return res.status(403).json({ message: 'Sem permissão' });
-
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
-}
+};
 
 exports.getById = async (req, res) => {
   try {
